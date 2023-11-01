2d494b502f9ed2ac8d2649e856709a517ef78373
jxs1805 <jxs1805@rit.edu>
2023-11-01 10:52:36 +0100

    minor additions

--- popde/density_estimate.py
+++ popde/density_estimate.py
@@ -2,30 +2,33 @@
 from awkde import GaussianKDE  #we will need our own code 
 
 
-class iterkde(object):
+class  densityestimator(object):
     """
-    a class that given choices of bw and smoothing parameters
-    compute FOM and best params for KDE and provide KDE on best 
-    params
+    A class that given input values of observations
+    and choices of bw  for each dimenion of input data
+    compute density estimator  with bandwoth optimized 
+    using cross validation
     """
-    def __init__(self, bw, alpha, minX, maxX):
+    def __init__(self, data, bw, alpha, minX, maxX, paramflag='log'):
+        self.data = data
         self.bw = bw
-        self.alpha = alpha
+        slef.alpha = alpha
         self.minX = 0 #in multi-Dimensions we need to have list with each dimenions min val
         self.maxX =100
-
-    def def kde_awkde(self, x, x_grid, alpha , bandwidth, ret_kde=False):
+      
+    
+    def kde_awkde(self, x, x_grid, bandwidth, alpha, ret_kde=False):
         """Kernel Density Estimation with awkde 
         inputs:
-        x = training data 
+        x = training data [n_dimension] 
         x_grid = testing data
-        alp = smoothing factor for local bw
         bandwidth = global bw for kde
+        alpha =smoothing parameter
         kwargs:
         ret_kde optional 
-        if True kde will be output with estimated kde-values 
+            if True kde will be output with estimated kde-values 
         """
-        kde = GaussianKDE(glob_bw=gl_bandwidth, alpha=alp, diag_cov=True)
+        kde = GaussianKDE(glob_bw=gl_bandwidth)
         kde.fit(x[:, np.newaxis])
         if isinstance(x_grid, (list, tuple, np.ndarray)) == False:
             y = kde.predict(x_grid)
@@ -42,13 +45,31 @@ class iterkde(object):
         we need to add scipy.optimization for 
         finding best choice of bw and alpha
         """
-        if bwchoice not in ['silverman', 'scott']:
-        bwchoice = float(bwchoice) #bwchoice.astype(np.float) #for list
         fom = 0.0
         for i in range(len(sample)):
             leave_one_sample, miss_sample = np.delete(sample, i), sample[i]
             y = kde_awkde(leave_one_sample, miss_sample, alp=alphachoice, gl_bandwidth=bwchoice)
             fom += np.log(y)
         return fom
+    
+    def estimation(self, samplevalues, x_gridvalues, alphagrid, bwgrid, paramflag):
+        """
+        given input data compute kde with optimization
+        over the bandwidth and alpha using cross validation
+        return kde evaluated on desired domain
+        if paramflag are log use log of input values
+        to get KDE
+        """
+        if paramflag=='log':
+            samples = np.log(samplevalues)
+        figure_of_merit = {}
+        for gbw in bwgrid:
+        for alp in alphagrid:
+            FOM[(gbw, alp)] = loocv_awkde(sample, gbw, alp)
+        optvalues = max(FOM.items(), key=operator.itemgetter(1))[0]
+        optbw, optalpha  = optvalues[0], optvalues[1]
 
+        kdeval = kde_awkde(samples, x_gridvalues, alp=optalpha, gl_bandwidth=optbw)
+    return kdeval, optbw, optalpha 
 
+        
