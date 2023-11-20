7594d6aad82e50a21f1b6d2a7a90e6c88a42ecc9
Thomas Dent <thomas.dent@usc.es>
2023-11-20 16:37:05 +0100

    many bugfixes / reworking

--- popde/density_estimate.py
+++ popde/density_estimate.py
@@ -1,9 +1,9 @@
-class AdaptiveKDEWithBandWidthUsingLeaveOneOutCrossValidation():
+class AdaptiveKDELeaveOneOutCrossValidation():
     """
-    A class that given input values of observations and choices of
-    bw for each dimension of input data
-    compute density estimator with bandwidth optimized 
-    using cross validation
+    A class that given input values of observations and a choice of
+    bandwidth and adaptive parameter values, optimizes the parameters
+    using leave-one-out cross validation and evaluates the density 
+    estimate
 
     Methods:
        kde_awkde(evaluate_grid): train and fit a kde return kde object
@@ -12,74 +12,73 @@ class AdaptiveKDEWithBandWidthUsingLeaveOneOutCrossValidation():
 
     Data attributes:
         data: input data from observation [_ndimensions]
-        bw : bw choices [_ndimensions]
-        alpha: smoothing parameter for adaptive bandwidth
-        minX: min of values on which to evaluate kde
-        maxX: max of values on which to evaluate kde
-        kwarg: flag on parameter units
+        bw : bandwidth choices, sequence
+        alpha: adaptive parameter, sequence
+        kwarg: flag for log parameter
     Usage:
     >>> xvals = np.random.rand(5,5)
     >>> bwarr = np.arange(0, 1.1, 0.1)
     >>> densityestimator.__init__(xvals, bwarray)
     """
-    def __init__(self, data, bw, alpha, minX, maxX, paramflag='log'):
+    def __init__(self, data, bw_arr, alpha_arr, paramflag=None):  # FIXME allow for log/non-log in different dimensions
         self.data = data
-        self.bw = bw
-        slef.alpha = alpha
-        self.minX = 0 #in multi-Dimensions we need to have list with each dimension min val
-        self.maxX =100
+        self.bw_arr = bw_arr
+        self.alpha_arr = alpha_arr
+        self.log_param = paramflag
+        self.optbw = None
+        self.optalpha = None
 
-    def kde_awkde(self, x, x_grid, bandwidth, alpha, ret_kde=False):
+    def train_eval_kde(self, x, x_grid, bandwidth, alpha, ret_kde=False):
         """Kernel Density Estimation with awkde 
         inputs:
-        x = training data [n_dimension] 
-        x_grid = testing data
-        bandwidth = global bw for kde
-        alpha =smoothing parameter
+        x: training data [n_dimension] 
+        x_eval: evaluation points
+        bandwidth: global bw for kde
+        alpha: smoothing parameter
         kwargs:
-        ret_kde optional 
+        ret_kde
             if True kde will be output with estimated kde-values 
         """
         from awkde import GaussianKDE  
         kde = GaussianKDE(glob_bw=gl_bandwidth)
         kde.fit(x[:, np.newaxis])
-        if isinstance(x_grid, (list, tuple, np.ndarray)) == False:
-            y = kde.predict(x_grid)
+        # Evaluate KDE at given points
+        if isinstance(x_eval, (list, tuple, np.ndarray)) == False:
+            y = kde.predict(x_eval)
         else:
-            y = kde.predict(x_grid[:, np.newaxis])
+            y = kde.predict(x_eval[:, np.newaxis])
 
         if ret_kde == True:
             return kde, y
         return y
 
-    def loocv_awkde(self, sample, bwchoice, alphachoice):
+    def loocv_awkde(self, sample, bw, alpha):
         """
-        use of leave one out cross validation for 
+        Calculate likelihood FORM using leave one out cross validation for 
         finding best choice of bandwidth and alpha
         """
-        fom = 0.0
+        fom = 0.
         for i in range(len(sample)):
             leave_one_sample, miss_sample = np.delete(sample, i), sample[i]
-            y = kde_awkde(leave_one_sample, miss_sample, alp=alphachoice, gl_bandwidth=bwchoice)
+            y = train_eval_kde(leave_one_sample, miss_sample, bw, alpha)
             fom += np.log(y)
         return fom
     
-    def estimation(self, samplevalues, x_gridvalues, alphagrid, bwgrid, paramflag):
+    def estimation(self, samples, x_eval, alphagrid, bwgrid):
         """
         given input data compute kde with optimization
         over the bandwidth and alpha using cross validation
         return kde evaluated on desired domain
-        if paramflag are log use log of input values
-        to get KDE
+        if paramflag are log, train KDE on log of input values
         """
-        if paramflag=='log':
-            samples = np.log(samplevalues)
-        figure_of_merit = {}
-        for gbw in bwgrid:
+        if self.log_param == 'log':  # FIXME: allow different dimensions with/without log
+            samples = np.log(samples)
+        fom = {}  # Dictionary holding optimization figure of merit
+        for bw in bwgrid:
             for alp in alphagrid:
-                FOM[(gbw, alp)] = loocv_awkde(sample, gbw, alp)
-        optvalues = max(FOM.items(), key=operator.itemgetter(1))[0]
-        optbw, optalpha  = optvalues[0], optvalues[1]
+                fom[(gbw, alp)] = loocv_awkde(samples, bw, alp)
+        optvalues = max(fom.items(), key=operator.itemgetter(1))[0]
+        self.optbw, self.optalpha = optvalues[0], optvalues[1]
+        kdeval = train_eval_kde(samples, x_eval, self.optbw, self.optalpha)
 
-        kdeval = kde_awkde(samples, x_gridvalues, alp=optalpha, gl_bandwidth=optbw)
-    return kdeval, optbw, optalpha 
+    return kdeval, self.optbw, self.optalpha 
