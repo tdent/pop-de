bd5811060e9e15828ee1e8325591e52ce8d30a6a
jxs1805 <jxs1805@rit.edu>
2024-03-01 18:33:27 +0100

    add a new function for mesh grid

--- popde/utils_plot.py
+++ popde/utils_plot.py
@@ -18,3 +18,17 @@ def simple2Dplot(xx, yy, kde):
 
     return fig
 
+def get_twoD_grid(data_dim1, data_dim2, num_points=100):
+    """
+    return mesh grid of data 
+    in two dimensions x, y 
+    return  xx, yy
+    """
+
+    dim1_min, dim1_max = np.min(data_dim1), np.max(data_dim1)
+    dim2_min, dim2_max = np.min(data_dim2), np.max(data_dim2)
+    dim1_grid = np.linspace(dim1_min, dim1_max, num_points)
+    dim2_grid = np.linspace(dim2_min, dim2_max, num_points)
+    xx, yy = np.meshgrid(dim1_grid, dim2_grid)
+    return xx, yy
+
