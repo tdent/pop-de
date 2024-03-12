dcb7d10180b96ccb63f4bee0b02609bff379cce0
jxs1805 <jxs1805@rit.edu>
2024-03-12 18:41:22 +0100

    added option for grid boundaries with pads and given options

--- popde/utils_plot.py
+++ popde/utils_plot.py
@@ -26,14 +26,32 @@ def simple2Dplot(xx, yy, kdevals, xlabel=None, ylabel=None, title=None, show_plo
     return fig
 
 
-def get_twoD_grid(data_dim1, data_dim2, num_points=100):
+def get_twoD_grid(data_dim1, data_dim2, num_points=100, pad=None, dim1_range=None, dim2_range=None):
     """
     return mesh grid of data 
     in two dimensions x, y 
     return  xx, yy
     """
-    dim1_min, dim1_max = np.min(data_dim1), np.max(data_dim1)
-    dim2_min, dim2_max = np.min(data_dim2), np.max(data_dim2)
+    if dim1_range is not None:
+        dim1_min, dim1_max = dim1_range
+    else:
+        dim1_min, dim1_max = np.min(data_dim1), np.max(data_dim1)
+
+    if dim2_range is not None:
+        dim2_min, dim2_max = dim2_range
+    else:
+        dim2_min, dim2_max = np.min(data_dim2), np.max(data_dim2)
+
+    if pad is not None:
+        dim1_range = dim1_max - dim1_min
+        dim2_range = dim2_max - dim2_min
+
+        dim1_min -= pad * dim1_range
+        dim1_max += pad * dim1_range
+
+        dim2_min -= pad * dim2_range
+        dim2_max += pad * dim2_range
+
     dim1_grid = np.linspace(dim1_min, dim1_max, num_points)
     dim2_grid = np.linspace(dim2_min, dim2_max, num_points)
     xx, yy = np.meshgrid(dim1_grid, dim2_grid)
