5959ce7809358c6e7ac656efad037b65f4289bed
jam sadiq <jsadiq@sissa.it>
2024-03-15 17:06:23 +0100

    fixed the title of plot

--- popde/density_estimate.py
+++ popde/density_estimate.py
@@ -224,7 +224,7 @@ class SimpleKernelDensityEstimation:
 
         # Create the contour plot
         zz = z.reshape(xx.shape)
-        fig = utils_plot.simple2Dplot(xx, yy, zz, xlabel=dim1, ylabel=dim2, title='2D Contour Plot of KDE for {dim1} and {dim2} (Sliced along {slice_dims})')
+        fig = utils_plot.simple2Dplot(xx, yy, zz, xlabel=dim1, ylabel=dim2, title=f'KDE sliced along {slice_dims} at {slice_values})')
 
         if file_name is not None:
             fig.savefig(file_name)
