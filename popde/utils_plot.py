1cab356399954bbce188187cc69c1940b91f08a9
Jam Sadiq <jsadiq@sissa.it>
2024-03-15 17:21:27 +0100

    started a module for plotting (#12)
    * started a module for ploting for issue #9
    
    * added a KDE plotter class and also a plot method in main KDE module
    
    * excess line
    
    * excess line
    
    * remove old plot method and add a new method
    
    * make  many modification to have a plotting method on simpleKDE class
    
    * removed extra classes in module
    
    * Example doc (#11)
    
    * remove non used brackets for example in doc
    
    * fixed some typos and make minor changes in using example
    
    * fixed using min max for eval points and also fixed the reshape to flatten
    
    * Example fixes
    
    Fix indentation
    Use .min rather than np.amin (shorter)
    Don't define unnecessary variables
    Fix grid_pts vs eval_pts bug
    Use dimension names for axis labels
    
    * make m1 > m2, use 'nearest' colormesh option
    
    * comment on colormesh option
    
    ---------
    
    Co-authored-by: Thomas Dent <thomas.dent@usc.es>
    
    * fixed minor comments, remove axis labels and add a return in plot function
    
    * add a new function for mesh grid
    
    * import utils_plot and get meshgrid
    
    * added axis labels for simple2D plot'
    
    * added labels in 2D contour plot
    
    * added a dimensionality check for slice plot
    
    * fixed filename for saving plot
    
    * remove color and alpha
    
    * fix a value error comment
    
    * remove extra check in if statement
    
    * remove extra as in import
    
    * remove extra comment
    
    * whitespace
    
    * whitespace
    
    * added showing of plot and fixed a minor bug f wrong name
    
    * fixed incorrect kde func call
    
    * changes to get corrected sliced plot
    
    * replace comment for example with updated code
    
    * script tp test plot script
    
    * add an extra error in plotting method
    
    * rearrange some lines of code
    
    * added option for grid boundaries with pads and given options
    
    * whitespace
    
    * fixed the pad to be 0 as default
    
    * fixed the correct evaluate function
    
    * fix two bugs self.data was not initiated before it is called and a miss spell
    
    * fixed plot_name in argument as added it in dicstring
    
    * fixed a comment
    
    * fix spacing in lines and removing extra comment
    
    * moved check statements before generating grids
    
    * removed extra line between two classes
    
    * fixed the title of plot
    
    ---------
    
    Co-authored-by: Thomas Dent <thomas.dent@usc.es>

---
+++ popde/utils_plot.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def simple2Dplot(xx, yy, kdevals, xlabel=None, ylabel=None, title=None, show_plot=False):
+    """
+    return kde plot in 2D given output
+    """
+    fig = plt.figure()
+    ax = fig.gca()
+    # Filled Contour plot
+    cfset = ax.contourf(xx, yy, kdevals, cmap='Blues')
+    # Contour plot
+    cset = ax.contour(xx, yy, kdevals, colors='k')
+    # Contour line labels
+    ax.clabel(cset, inline=1, fontsize=10)
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    if title:
+        ax.set_title(title)
+    if show_plot==True:
+        plt.show()
+    return fig
+
+
+def get_twoD_grid(data_dim1, data_dim2, num_points=100, pad=0.0, dim1_range=None, dim2_range=None):
+    """
+    return mesh grid of data 
+    in two dimensions x, y 
+    return  xx, yy
+    """
+    if dim1_range is not None:
+        dim1_min, dim1_max = dim1_range
+    else:
+        dim1_min, dim1_max = np.min(data_dim1), np.max(data_dim1)
+
+    if dim2_range is not None:
+        dim2_min, dim2_max = dim2_range
+    else:
+        dim2_min, dim2_max = np.min(data_dim2), np.max(data_dim2)
+
+    dim1_range = dim1_max - dim1_min
+    dim2_range = dim2_max - dim2_min
+
+    dim1_min -= pad * dim1_range
+    dim1_max += pad * dim1_range
+
+    dim2_min -= pad * dim2_range
+    dim2_max += pad * dim2_range
+
+    dim1_grid = np.linspace(dim1_min, dim1_max, num_points)
+    dim2_grid = np.linspace(dim2_min, dim2_max, num_points)
+    xx, yy = np.meshgrid(dim1_grid, dim2_grid)
+    return xx, yy
+
