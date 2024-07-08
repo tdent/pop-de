5f9db179f04d37fdf31cbc892c0752129a34338c
jam sadiq <jsadiq@sissa.it>
2024-07-08 17:56:26 +0200

    fixed using numpy and corrected ndim in weight option

--- popde/density_estimate.py
+++ popde/density_estimate.py
@@ -79,10 +79,10 @@ class SimpleKernelDensityEstimation:
         self.weights = weights
         if self.weights is not None:
             # Check the array
-            self.weights = atleast_1d(weights).astype(float)
+            self.weights = np.atleast_1d(weights).astype(float)
             if self.weights.ndim != 1:
                 raise ValueError("weights should be one-dimensional.")
-            if len(self.weights) != self.data.shape[1]:
+            if len(self.weights) != self.data.shape[0]:
                 raise ValueError("weights should be of length of input data")
             # Normalize to sum to 1
             self.weights /= self.weights.sum()
